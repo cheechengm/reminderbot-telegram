b16a1ead6ff4ba65a622e1fd073cbb623aa516b9
chee cheng <cheechengdragneel@gmail.com>
2026-03-30 09:28:50 +0800

    deleted

--- routes/webhook.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request
-
-router = APIRouter()
-
-@router.post("/{token}")
-async def handler(request: Request):
-    update = await request.json()
-    await tg_app.update_queue.put(update)
-    return {"ok": True}
